7e52d5caf150ad89a93e683309ca1d3734b85d3b
Wouter Meuwis <woutermeuwis@gmail.com>
2022-12-06 20:05:42 +0100

    Day 6
    Meesa big dumdum

---
+++ Day06/Program.cs
@@ -0,0 +1,7 @@
+﻿// read input
+var input = (await File.ReadAllTextAsync("input.txt")).ToCharArray();
+
+Console.WriteLine($"Part 1: {GetMarkerPosition(input, 4)}");
+Console.WriteLine($"Part 1: {GetMarkerPosition(input, 14)}");
+
+int GetMarkerPosition(char[] arr, int length) => Enumerable.Range(0, arr.Length - length).FirstOrDefault(i => arr.Skip(i).Take(length).Distinct().Count() == length) + length;
