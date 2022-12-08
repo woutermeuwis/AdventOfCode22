50fb6569601622282e74ef4df52ff3a7ff60ffff
Wouter Meuwis <woutermeuwis@gmail.com>
2022-12-08 09:37:01 +0100

    Day 8

---
+++ Day08/Program.cs
@@ -0,0 +1,42 @@
+﻿var input = (await File.ReadAllLinesAsync("input.txt")).Select(line => line.ToCharArray()).ToArray();
+
+var visibleTrees = 0;
+var highestScenic = 0;
+
+for (var y = 0; y < input.Length; y++)
+{
+    var row = input[y];
+    for (var x = 0; x < input[y].Length; x++)
+    {
+        var col = input.Select(r => r[x]).ToArray();
+        var tree = row[x];
+        var left = row[..x];
+        var up = col[..y];
+        var right = row[(x + 1)..];
+        var down = col[(y + 1)..];
+
+        if (tree == 0) continue;
+
+        if (y == 0 || y == input.Length - 1 || x == 0 || x == row.Length - 1
+            || new[] { left.Max(), right.Max(), up.Max(), down.Max() }.Min() < tree)
+            visibleTrees++;
+
+        int GetIndexOfHigherThen(char[] arr, int treshold)
+        {
+            if (arr.Any())
+                for (var i = 0; i < arr.Length; i++)
+                    if (arr[i] >= treshold) return ++i;
+            return arr.Length;
+        }
+
+        var sceneL = GetIndexOfHigherThen(left.Reverse().ToArray(), tree);
+        var sceneU = GetIndexOfHigherThen(up.Reverse().ToArray(), tree);
+        var sceneR = GetIndexOfHigherThen(right, tree);
+        var sceneD = GetIndexOfHigherThen(down, tree);
+
+        highestScenic = Math.Max(highestScenic, sceneL * sceneU * sceneR * sceneD);
+    }
+}
+
+Console.WriteLine($"Part 1: {visibleTrees}");
+Console.WriteLine($"Part 2: {highestScenic}");
