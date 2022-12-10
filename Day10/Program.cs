31a896dca40659a2d3052d2c72075db64eba8ccd
Wouter Meuwis <wouter.meuwis@3factr.be>
2022-12-10 09:05:52 +0100

    Day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+var input = await File.ReadAllLinesAsync("input.txt");
+int X = 1;
+var Register = new List<int> { 0 };
+
+foreach (var line in input)
+{
+    if (line == "noop")
+    {
+        Register.Add(X);
+    }
+    else
+    {
+        Register.Add(X);
+        Register.Add(X);
+        X += int.Parse(line.Split(' ')[1]);
+    }
+}
+
+var part1 = 0;
+for (var i = 20; i < Register.Count; i += 40)
+    part1 += Register[i] * i;
+
+var CRT = new StringBuilder();
+for (var i = 1; i <= 240; i++)
+{
+    var pos = Register[i];
+    CRT.Append(Math.Abs(pos - ((i % 40) - 1)) <= 1 ? '#' : '.');
+    if (i % 40 == 0)
+        CRT.AppendLine();
+}
+
+Console.WriteLine($"Part 1: {part1}");
+Console.Write(CRT.ToString());
